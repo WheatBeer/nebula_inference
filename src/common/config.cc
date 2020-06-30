4f634e7a66d53bf24b6e514773509d77dfdf7e9d
WheatBeer <sungminryu94@gmail.com>
2020-07-01 00:46:36 +0900

    tab to space

--- src/common/config.cc
+++ src/common/config.cc
@@ -39,7 +39,7 @@ void config_t::parse(std::string m_config_name) {
 		std::cerr << "Error: failed to open " << m_config_name << std::endl; exit(1);
     }
 
-	std::string line;
+    std::string line;
     while(getline(file_stream, line)) {
         // Erase all spaces
         line.erase(remove(line.begin(),line.end(),' '),line.end());
@@ -47,17 +47,17 @@ void config_t::parse(std::string m_config_name) {
         if(!line.size() || (line[0] == '#')) continue;
         // Beginning of [section]
         if(line[0] == '[') {
-			std::string section_name = line.substr(1, line.size()-2);
+            std::string section_name = line.substr(1, line.size()-2);
             sections.push_back(section_config_t(section_name));
         }
         else {
             size_t eq = line.find('=');
             if(eq == std::string::npos) {
-				std::cerr << "Error: invalid config" << std::endl << line << std::endl; exit(1);
+                std::cerr << "Error: invalid config" << std::endl << line << std::endl; exit(1);
             }
             // Save (key, value) pair in the latest section setting.
-			std::string key = line.substr(0, eq);
-			std::string value = line.substr(eq + 1, line.size() - 1);
+            std::string key = line.substr(0, eq);
+            std::string value = line.substr(eq + 1, line.size() - 1);
             sections[sections.size() - 1].add_setting(key, value);
         }
     }
