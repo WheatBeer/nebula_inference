9f344482863851bdc8d0146cbacea568de868fd2
WheatBeer <sungminryu94@gmail.com>
2020-07-01 00:38:39 +0900

    tab to space

--- datasets/mklst.sh
+++ datasets/mklst.sh
@@ -7,13 +7,13 @@ datasetdir=$PWD
 
 # Create dataset lists.
 for d in $datasetdir/*; do
-	if [ -d $d ]; then
-		dataset=$(basename $d)
-		echo -e "# Creating $dataset dataset lists ..."
-		if [[ $dataset = 'mnist' ]]; then
-			cd $d; eval find $d/test -name  \*.jpg > test.lst
-		elif [[ $dataset = 'imagenet' ]]; then	
-			cd $d; eval find $d/test -name  \*.JPEG > test.lst
-		fi
-	fi
+    if [ -d $d ]; then
+        dataset=$(basename $d)
+        echo -e "# Creating $dataset dataset lists ..."
+        if [[ $dataset = 'mnist' ]]; then
+            cd $d; eval find $d/test -name  \*.jpg > test.lst
+        elif [[ $dataset = 'imagenet' ]]; then	
+            cd $d; eval find $d/test -name  \*.JPEG > test.lst
+        fi
+    fi
 done
